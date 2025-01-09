3affa99cb7a2c99ba21243a1ffdf50c002cdbe80
Shriram Saravanan <shriram.saravanan@solitontech.local>
2025-01-09 16:55:24 +0530

    Update : gitKeep

--- ContactManager/ContactManager/obj/Debug/net8.0/ContactManager.AssemblyInfo.cs
+++ ContactManager/ContactManager/obj/Debug/net8.0/ContactManager.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ContactManager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3fec6beb5441b9f3b38a0103cb090e6e4349f14f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b35dbb94077bb49dad8b4b5ef3d7e350ef5115f4")]
 [assembly: System.Reflection.AssemblyProductAttribute("ContactManager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ContactManager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
